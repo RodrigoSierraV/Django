4286f51d4b74d628bdb7fcb5ae53c124ad4950bc
RodrigoSierraV <735@holbertonschool.com>
2020-03-22 23:13:09 -0500

    [Add] model post and allow media in admin

--- course/platzigram/platzigram/urls.py
+++ course/platzigram/platzigram/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as post_views
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', local_views.hello),
     path('sorted/', local_views.sorted_num),
     path('hi/<str:name>/<int:age>/', local_views.hi),
     path('posts/', post_views.list_posts)
-]
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- course/platzigram/users/admin.py
+++ course/platzigram/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from users.models import Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import User
 # Register your models here.
 
 #admin.site.register(Profile)
@@ -17,3 +19,43 @@ class ProfileAdmin(admin.ModelAdmin):
     )
 
     list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                ('user', 'picture'),
+                
+            ),
+        }),
+        ('Extra Info', {
+            'fields': (
+                ('phone_number', 'website'),
+                ('biography')
+            ),
+        }),
+        ('Meta Data', {
+            'fields': (('created', 'modified'),),
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
