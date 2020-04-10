369a5105118435841e865beddc98240ba5e5459f
RodrigoSierraV <735@holbertonschool.com>
2020-04-09 20:22:21 -0500

    [Add] circle creator as member. Validation to circle's limit

--- cride/cride/circles/serializers/circles.py
+++ cride/cride/circles/serializers/circles.py
@@ -5,6 +5,20 @@ from cride.circles.models import Circle
 
 class CircleModelSerializer(serializers.ModelSerializer):
 
+    members_limit = serializers.IntegerField(
+        required=False,
+        min_value=5,
+        max_value=3200
+    )
+
+    is_limited = serializers.BooleanField(default=False)
+
+    read_only_fields = (
+        'is_public',
+        'verified',
+        'rides_offered',
+        'rides_taken'
+    )
     class Meta:
         
         model = Circle
@@ -15,3 +29,12 @@ class CircleModelSerializer(serializers.ModelSerializer):
             'verified', 'is_public',
             'is_limited', 'members_limit'
         )
+
+    def validate(self, data):
+        """Ensures members limit"""
+        members_limit = data.get('members_limit', None)
+        is_limited = data.get('members_limit', None)
+
+        if bool(members_limit) ^ is_limited:
+            raise serializers.ValidationError("If circle is limited a member's limit must be provided")
+        return data
--- cride/cride/circles/views/circles.py
+++ cride/cride/circles/views/circles.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from cride.circles.serializers import CircleModelSerializer
 
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
 
 class CircleViewSet(viewsets.ModelViewSet):
@@ -17,3 +17,18 @@ class CircleViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return queryset.filter(is_public=True)
         return queryset
+    
+    def perform_create(self, serializer):
+        """Assigns circle admin"""
+
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitations=10
+        )
