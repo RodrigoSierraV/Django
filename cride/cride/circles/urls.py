ca498ca83ec44c58bb0924199e0930b2dea6df37
vagrant <vagrant@vagrant.vm>
2020-04-11 19:50:33 +0000

    [Add] list circle members

--- cride/cride/circles/urls.py
+++ cride/cride/circles/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from cride.circles.views import circles as circle_views
-
+from cride.circles.views import memberships as membership_views
 
 router = DefaultRouter()
 router.register(r'circles', circle_views.CircleViewSet, basename='circle')
-
+router.register(
+    r'circles/(?P<slug_name>[-a-zA-Z0-9_]+)/members',
+    membership_views.MembershipViewSet,
+    basename='membership'
+)
 urlpatterns = router.urls
---
+++ cride/cride/circles/views/memberships.py
@@ -0,0 +1,26 @@
+from rest_framework import mixins, viewsets
+from rest_framework.generics import get_object_or_404
+
+from cride.circles.models import Circle
+from cride.circles.models import Membership
+from cride.circles.serializers import MembershipModelSerializer
+
+
+class MembershipViewSet(mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+
+    serializer_class = MembershipModelSerializer
+    def dispatch(self, request, *args, **kwargs):
+        """Verify that the circle exits"""
+
+        slug_name = kwargs['slug_name']
+        self.circle = get_object_or_404(
+            Circle, slug_name=slug_name
+        )
+        return super(MembershipViewSet, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        """Return circle members"""
+        return Membership.objects.filter(
+
+        )
